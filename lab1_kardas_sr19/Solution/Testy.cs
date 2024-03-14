3aec31e3e546dad8be5f49cba2d8a0a5aad86308
Kasjano <161947327+Kasjano@users.noreply.github.com>
2024-03-14 14:22:31 +0100

    Add files via upload

---
+++ lab1_kardas_sr19/Solution/Testy.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem_plecakowy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public class Testy
+    {
+        [TestMethod]
+        public void KolejnoscPrzedmiotowWplywaNaRozwiazanie()
+        {
+            Problem problem = new Problem(0, 1);
+            Problem problem2 = new Problem(0, 1);
+            problem.AddItemManually(1, 2, 2);
+            problem.AddItemManually(2, 4, 5);
+            problem.AddItemManually(3, 2, 3);
+            problem2.AddItemManually(1, 4, 5);
+            problem2.AddItemManually(2, 2, 3);
+            problem2.AddItemManually(3, 2, 2);
+            int capacity = 8;
+            //act
+            Result result = problem.Solve(capacity);
+            Result result2 = problem2.Solve(capacity);
+            //assert
+            Assert.AreEqual(result.TotalValue, result2.TotalValue);
+            Assert.AreEqual(result.TotalWeight, result2.TotalWeight);
+        }
+
+        [TestMethod]
+        public void PrzynajmniejJedenPrzedmiotSpelniaOgraniczenia()
+        {
+            Problem problem = new Problem(0, 1);
+            problem.AddItemManually(1, 2, 1);
+            int capacity = 1;
+            //act
+            Result result = problem.Solve(capacity);
+            //assert
+            Assert.IsTrue(result.Numbers.Count > 0);
+        }
+
+        [TestMethod]
+        public void SprawdzenieDzialaniaRecznegoDodawaniaPrzedmiotowDoListy()
+        {
+            Problem problem = new Problem(0, 1);
+            //act
+            problem.AddItemManually(1, 2, 3);
+            //assert
+            Assert.AreEqual(1, problem.Items.Count);
+            Assert.AreEqual(1, problem.Items[0].Number);
+            Assert.AreEqual(2, problem.Items[0].Value);
+            Assert.AreEqual(3, problem.Items[0].Weight);
+        }
+
+        [TestMethod]
+        //test sprawdza dzialanie programu gdy na liscie przedmiotow znajduja sie przedmioty
+        //o tej samej wartosci i wadze
+        public void SprawdzenieDzialaniaDlaKilkuPrzedmiotowOTejSamejWadzeIWartosci()
+        {
+            Problem problem = new Problem(0, 1);
+            problem.AddItemManually(1, 2, 2);
+            problem.AddItemManually(2, 2, 2);
+            problem.AddItemManually(3, 2, 2);
+            int capacity = 5;
+            //act
+            Result result = problem.Solve(capacity);
+            //assert
+            Assert.AreEqual(result.TotalValue, 4);
+            Assert.AreEqual(result.TotalWeight, 4);
+        }
+
+        [TestMethod]
+        public void UjemnaPojemnoscPlecaka()
+        {
+            Problem problem = new Problem(10, 1);
+            //act
+            Result result = problem.Solve(-5);
+            //assert
+            Assert.IsTrue(result.Numbers.Count == 0);
+            Assert.IsTrue(result.TotalValue == 0);
+            Assert.IsTrue(result.TotalWeight == 0);
+        }
+
+        [TestMethod]
+        public void WszystkiePrzeedmiotyMieszczaSieWPlecaku()
+        {
+            Problem problem = new Problem(0, 1);
+            //act
+            problem.AddItemManually(1, 2, 2);
+            problem.AddItemManually(2, 4, 5);
+            problem.AddItemManually(3, 2, 3);
+            Result result = problem.Solve(10);
+            //assert
+            Assert.AreEqual(3, result.Numbers.Count);
+            Assert.AreEqual(8, result.TotalValue);
+            Assert.AreEqual(10, result.TotalWeight);
+        }
+
+        [TestMethod]
+        public void WynikKonkretnejInstancji()
+        {
+            Problem problem = new Problem(0, 1);
+            problem.AddItemManually(1, 2, 2);
+            problem.AddItemManually(2, 4, 5);
+            problem.AddItemManually(3, 2, 3);
+            int capacity = 8;
+            //act
+            Result result = problem.Solve(capacity);
+            //assert
+            Assert.AreEqual(result.TotalValue, 6);
+            Assert.AreEqual(result.TotalWeight, 7);
+        }
+
+        [TestMethod]
+        public void ZadenPrzedmiotNieSpelniaOgraniczen()
+        {
+            Problem problem = new Problem(10, 1);
+            int capacity = 0;
+            //act
+            Result result = problem.Solve(capacity);
+            //assert
+            Assert.IsTrue(result.Numbers.Count == 0);
+        }
+    }
+}
+
---
+++ lab1_kardas_sr19/Solution/UnitTestProblem.cs
@@ -0,0 +1,22 @@
+﻿using Problem_plecakowy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTestProblem
+    {
+        [TestMethod]
+        public void TestMethodCountElements(){
+            List<int> sizes = new List<int>() { 10, 20, 30, 40, 50 };
+            foreach (int n in sizes ){
+                Problem problem = new Problem(n,1);
+                Assert.AreEqual(n, problem.Items.Count);
+            }
+        }
+    }
+}
